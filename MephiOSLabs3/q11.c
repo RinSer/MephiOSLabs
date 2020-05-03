7917f562c42a1b5383a756b2555437b6d6388197
Sergei Irin <v-seirin@microsoft.com>
2020-05-03 21:58:04 +0300

    Lab3 completed

--- MephiOSLabs3/q11.c
+++ MephiOSLabs3/q11.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <signal.h>
+#include <sys/types.h>
 #include <unistd.h>
 
 /*
@@ -8,32 +9,55 @@
 Выполнить эту программу и объяснить ее поведение. Использовать sigsuspend и sigprocmask.
 */
 
-void onSignal11(int);
+volatile sig_atomic_t no_interrupt = 1;
+void sync_signal(int);
 
 int q11()
 {
     printf("=== question 11 start ===\n\n");
 
-    struct sigaction newAction;
-    if (sigfillset(&newAction.sa_mask) < 0)
-        return catch ();
-    newAction.sa_handler = onSignal11;
-    newAction.sa_flags = 0;
+    int signal2wait = SIGUSR1;
 
-    if (sigaction(SIGINT, &newAction, NULL) < 0)
-        return catch ();
+    int pid = fork();
 
-    pause();
+    if (pid > 0)
+    {
+        struct sigaction on_signal;
+        if (sigfillset(&on_signal.sa_mask) < 0)
+            return catch();
+        on_signal.sa_handler = sync_signal;
+        on_signal.sa_flags = 0;
+        if (sigaction(signal2wait, &on_signal, NULL) < 0)
+            return catch();
+
+        sigset_t mask;
+        /*if (sigfillset(&mask) +
+            sigdelset(&mask, signal2wait) < 0)
+            return catch();*/
+        if (sigprocmask(SIG_BLOCK, NULL, &mask) < 0)
+            return catch();
+
+        while (no_interrupt)
+            sigsuspend(&mask);
+    }
+    else 
+    {
+        for (int i = 1; i < 11; i++)
+        {
+            printf("Child iteration number %d\n", i);
+            sleep(1);
+        }
+        kill(getppid(), signal2wait);
+        exit(0);
+    }
+
+    printf("\n==== question 11 end ====\n");
 
     return 0;
 }
 
-void onSignal11(int signum) {
-    printf("Sleeping on signal %d\n", signum);
-
-    sleep(5);
-
-    printf("\n==== question 11 end ====\n");
-
-    exit(0);
+void sync_signal(int signum)
+{
+    printf("Parent proceeds after wait on signal %d\n", signum);
+    no_interrupt = 0;
 }
