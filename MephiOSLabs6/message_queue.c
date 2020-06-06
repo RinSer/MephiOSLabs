c13bb68e837b30efeb29f6f676fd0ca368aaed36
Sergei Irin <v-seirin@microsoft.com>
2020-06-06 23:17:59 +0300

    Number of messages in queue check on send

--- MephiOSLabs6/message_queue.c
+++ MephiOSLabs6/message_queue.c
@@ -39,10 +39,26 @@ int get_from_queue(int qid, int type)
 
 void send_to_queue(int qid, int number, int type)
 {
+    struct msqid_ds* queue_stats = malloc(sizeof(struct msqid_ds));
+    msgctl(qid, IPC_STAT, queue_stats);
+    int num_messages = queue_stats->msg_qnum;
+
     game_message* msg = malloc(sizeof(game_message));
     sprintf(msg->payload, "%d", number);
     msg->type = type;
     msgsnd(qid, msg, sizeof(game_message), NULL);
+
+    msgctl(qid, IPC_STAT, queue_stats);
+    int num_messages_after = queue_stats->msg_qnum;
+    while (num_messages == num_messages_after)
+    {
+        msgsnd(qid, msg, sizeof(game_message), NULL);
+        msgctl(qid, IPC_STAT, queue_stats);
+        int num_messages_after = queue_stats->msg_qnum;
+    }
+
     free(msg);
+    free(queue_stats);
+
     catch("Error sending to queue");
 }
