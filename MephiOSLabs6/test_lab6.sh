712cb95217f24e3d03f88ba044c48bcaeb103702
Sergei Irin <v-seirin@microsoft.com>
2020-06-07 13:26:01 +0300

    Test script modified

--- MephiOSLabs6/test_lab6.sh
+++ MephiOSLabs6/test_lab6.sh
@@ -1,10 +1,10 @@
 # Скрипт для тестирования программы,
 # должен запускаться в одной папке с бинарником
-for n in {2..100}
+for n in $(seq 2 100)
 do
-  for m in {1..1000000}
+  for m in $(seq 1 1000000)
   do
-    for l in {1..100}
+    for l in $(seq 1 100)
     do
       ./MephiOSLabs6.out $n $m $l
     done
